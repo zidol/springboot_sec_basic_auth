028f740ad1a03e9c4f7f071fe56a58f48058787d
zidol <ajh3166@naver.com>
2021-06-28 10:37:21 +0900

    basic auth for spa

---
+++ server/login-multi-chain/src/test/java/com/sp/fc/web/MultiChainProxyTest.java
@@ -0,0 +1,57 @@
+package com.sp.fc.web;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sp.fc.web.student.Student;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
+
+
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MultiChainProxyTest {
+
+    @LocalServerPort
+    int port;
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+
+    @DisplayName("1. 학생 조사")
+    @Test
+    void test_1() throws JsonProcessingException {
+        String url = format("http://localhost:%d/api/teacher/students", 9054);
+        System.out.println("url = " + url);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.AUTHORIZATION, "basic "+ Base64.getEncoder().encodeToString("choi:1".getBytes()));
+
+        HttpEntity<String> entity = new HttpEntity<>("", httpHeaders);
+        ResponseEntity<String> response = restTemplate.exchange(url,
+                HttpMethod.GET, entity, String.class);
+
+
+        System.out.println("response.getBody() = " + response.getBody());
+//        List<Student> list = new ObjectMapper().readValue(response.getBody(),
+//                new TypeReference<List<Student>>() {});
+//
+//        System.out.println("list = " + list);
+//        assertEquals(3, list.size());
+    }
+
+}
