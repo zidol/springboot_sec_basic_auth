795a15bf9474efc84565e4df5f1ec0bc84dbb8b1
zidol <ajh3166@naver.com>
2021-06-29 01:31:39 +0900

    데이터베이스에서 정보를 가져와 로그인

---
+++ comp/user-admin/src/main/java/com/sp/fc/user/domain/SpUser.java
@@ -0,0 +1,67 @@
+package com.sp.fc.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "sp_user")
+public class SpUser implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL) //user와 라이프사이클이 같음
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "user_id"))
+    private Set<SpAuthority> authorities;
+
+    private String email;
+
+    private String password;
+
+    private boolean enabled;
+
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return null;
+//    }
+
+//    @Override
+//    public String getPassword() {
+//        return null;
+//    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return enabled;
+    }
+
+//    @Override
+//    public boolean isEnabled() {
+//        return false;
+//    }
+}
