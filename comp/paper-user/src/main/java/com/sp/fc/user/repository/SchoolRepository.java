a3fb47505e59f619c9c2da9a25c3da816456f9b1
zidol <ajh3166@naver.com>
2021-07-04 15:00:47 +0900

    테스트 환경 구성,  School 도메인 생성, 로직 개발, 테스트

---
+++ comp/paper-user/src/main/java/com/sp/fc/user/repository/SchoolRepository.java
@@ -0,0 +1,15 @@
+package com.sp.fc.user.repository;
+
+import com.sp.fc.user.domain.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface SchoolRepository extends JpaRepository<School, Long> {
+
+    @Query("select distinct(city) from School")
+    List<String> getCities();
+
+    List<School> findAllByCity(String city);
+}
